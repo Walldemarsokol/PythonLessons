111adb6dc2c7180160f07a25cca9c4139709ca44
VladimirSokol <walldemarsokol@yandex.ru>
2022-10-17 23:54:51 +0300

    add_HomeWork_3

---
+++ HomeWork3/HomeWork3.py
@@ -0,0 +1,44 @@
+#Задача 1. Задайте список из нескольких чисел. Напишите программу, 
+# которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+
+# list=input('введите числа через пробел: ')
+# list =list.replace(' ','')
+# sum=0
+# for i in range(0,len(list)):
+#     if i%2!=0:
+#         n=int(list[i])
+#         sum= sum+n
+# print(f'Answer: {sum}')
+
+#Задача 2. Напишите программу, которая найдёт произведение пар чисел 
+# списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+# list=[]
+# for _ in range(3):
+#     list.append(input('введите числа через enter: '))
+
+# if 
+# # list_of_product =[]
+# # for i in range(0,len(list)//2):
+# #     for j in range(len(list)//2,-len(list)):
+        
+#         print(list)
+
+
+# Задача 3 Задайте список из вещественных чисел. Напишите программу, 
+# которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+
+# some_list=[]
+# another_list=[]
+# for n in range(3):
+#     some_list.append(input(f'введите число {n+1} через enter: '))
+#     i=float(some_list[n])
+#     x=round((i%1),2)
+#     another_list.append(x)
+# max_n=max(another_list)
+# min_n=min(another_list)
+# print(f"{max_n-min_n}")
+
+
+
+
