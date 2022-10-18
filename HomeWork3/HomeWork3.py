11e68bed44375ab2f181075dc228334b81873b46
walldemarsokol <walldemarsokol@yandex.ru>
2022-10-18 12:57:00 +0300

    add_get_done_HomeWork3

--- HomeWork3/HomeWork3.py
+++ HomeWork3/HomeWork3.py
@@ -10,24 +10,38 @@
 #         sum= sum+n
 # print(f'Answer: {sum}')
 
-#Задача 2. Напишите программу, которая найдёт произведение пар чисел 
+# Задача 2. Напишите программу, которая найдёт произведение пар чисел 
 # списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
-# list=[]
 # list_of_product =[]
-# n=int(input("Введите размер строк: "))
-# for _ in range(n):
+# n=int(input("Введите размер списка: "))
+# list=[]
+# for i in range(n):
 #     list.append(int(input('введите числа через enter: ')))
-#     for i in range(len(list)):
+# if  n%2==0:
+#     x=n-1
+#     z=int(n/2)
+#     a=0
+#     while a<n/2:
+#         for j in range(z):
+#             list_of_product.append(list[j]*list[x])
+#             # print(list[j])
+#             x-=1
+#             a+=1
+# if n%2!=0:
+#     x=n-1
+#     z=int(n//2+1)
+#     a=0
+#     while a<n//2+1:
+#         for j in range(z):
+#             list_of_product.append(list[j]*list[x])
+#             # print(list[j])
+#             x-=1
+#             a+=1
 
-
-
-# for i in range(0,len(list)//2):
-#     for j in range(len(list)//2,-len(list)):
-        
 # print(list)
-
-
+# print(list_of_product)
+    
 # Задача 3 Задайте список из вещественных чисел. Напишите программу, 
 # которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
 
@@ -43,5 +57,18 @@
 # print(f"{max_n-min_n}")
 
 
+#Задача4. Напишите программу, которая 
+# будет преобразовывать десятичное число в двоичное.
 
+# n=int(input())
+# m=n
+# p=1
+# d1=0
+# while m>0:
+#     d1=d1+m%2*p
+#     p=p*10
+#     m=m//2
+# print(d1)
 
+#или еще вариант с использованием синтаксиса
+# print(bin(int(input())))
