61d0f8436411c1616e882e27e60aee3d5d31219f
VladimirSokol <walldemarsokol@yandex.ru>
2022-10-21 22:40:15 +0300

    add_seminar4

---
+++ Seminar4/Test4.py
@@ -0,0 +1,13 @@
+# #задача 2 из дз.
+# count = int(input('Введите колиество элементов: '))
+# some_list = []
+# for _ in range( count):
+#     number = int(input())
+#     some_list.append(number)
+
+# new_list = []
+# for idx in range((count-1)//2+1):
+#     number1=some_list[idx]
+#     number2=some_list[count-idx -1]
+#     new_list.append(number1*number2)
+# print(new_list)
