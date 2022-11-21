8bc59c23be0f67ce9c1bff755e663705d477bccb
VladimirSokol <walldemarsokol@yandex.ru>
2022-11-21 19:25:02 +0300

    add home_work8 and redact WH7

---
+++ HomeWork8/export_data.py
@@ -0,0 +1,23 @@
+#модуль экспорта
+
+def export_data():
+    with open('phone.csv', 'r', encoding='utf-16') as file:
+        data = []
+        t = []
+        for line in file:
+            if ',' in line:
+                temp = line.strip().split(',')
+                data.append(temp)
+            elif ';' in line:
+                temp = line.splin().split(';')
+                data.append(temp)
+            elif ':' in line:
+                temp = line.splin().split(':')
+                data.append(temp)
+            elif line != '':
+                if line != '\n':
+                    t.append(line.strip())
+                else:
+                    data.append(t)
+                    t = []
+    return data
