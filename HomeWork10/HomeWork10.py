1f86ccbfe18abbb844f6571dd78cbcabb2a62927
walldemarsokol <walldemarsokol@yandex.ru>
2022-12-01 20:41:18 +0300

    add homework10

---
+++ HomeWork10/HomeWork10.py
@@ -0,0 +1,54 @@
+# class MinStat:
+#     def __init__(self):
+#         self.some_list=[]
+    
+#     def add_number(self,number):
+#         self.some_list.append(number)
+        
+#     def result(self):
+#         if self.some_list==[]:
+#             return None
+#         else:
+#             return min(self.some_list)
+    
+# class MaxStat:
+#     def __init__(self):
+#         self.some_list=[]
+    
+#     def add_number(self,number):
+#         self.some_list.append(number)
+        
+#     def result(self):
+#         if self.some_list==[]:
+#             return None
+#         else:
+#             return max(self.some_list)
+    
+# class AverageStat:
+#     def __init__(self):
+#         self.some_list=[]
+    
+#     def add_number(self,number):
+#         self.some_list.append(number)
+        
+#     def result(self):
+#         if self.some_list==[]:
+#             return None
+#         else:
+#             l=len(self.some_list)
+#             n=sum(self.some_list)
+#             return n/l
+
+
+# values = [1, 2, 4, 5]
+# mins = MinStat()
+# maxs = MaxStat()
+# average = AverageStat()
+# for v in values:
+#     mins.add_number(v)
+#     maxs.add_number(v)
+#     average.add_number(v)
+    
+# print(mins.result(), maxs.result(), '{:<05.3}'.format(average.result()))
+
+
